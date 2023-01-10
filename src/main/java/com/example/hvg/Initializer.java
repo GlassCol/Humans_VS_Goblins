c0cac8aa5534a7cd0603415f2a13ac79cd7914be
GlassCol <coleglass18@outlook.com>
2023-01-10 09:19:08 -0600

    Added Dynamic Names for Humans and Goblins

--- src/main/java/com/example/hvg/Initializer.java
+++ src/main/java/com/example/hvg/Initializer.java
@@ -4,8 +4,13 @@ import com.example.hvg.humanoid.Goblin;
 import com.example.hvg.humanoid.Human;
 import com.example.hvg.humanoid.Humanoid;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * This deals with initializing the game
@@ -15,6 +20,7 @@ import java.util.Random;
 public class Initializer {
     ArrayList<Humanoid> humanList = new ArrayList<>();
     ArrayList<Humanoid> goblinList = new ArrayList<>();
+
 
     /**
      * Initializes all humanoids in the game
@@ -28,13 +34,22 @@ public class Initializer {
         ArrayList<ArrayList<Humanoid>> humanoidList = new ArrayList<>();
         Random rand = new Random();
         for (int i = 0; i < numOfHumans; i++) {
-            Humanoid human = new Human(5, "Human" + i, "Pink", (10 + rand.nextInt(10)),
+            String name = getHumanName();
+            //check for name duplicates
+            while (doesNameExist(name, humanList)) {
+                name = getHumanName();
+            }
+            Humanoid human = new Human(5, name, "Pink", (10 + rand.nextInt(10)),
                     i, 0);
             humanList.add(human);
         }
         for (int i = 0; i < numOfGoblins; i++) {
-            Humanoid goblin = new Goblin(5, "Goblin" + i,
-                    "Green", (10 + rand.nextInt(10)), rand.nextInt((gameSize - 1)) + 1,
+            String name = getGoblinName();
+            while (doesNameExist(name, goblinList)) {
+                name = getGoblinName();
+            }
+            Humanoid goblin = new Goblin(5, name, "Green",
+                    (10 + rand.nextInt(10)), rand.nextInt((gameSize - 1)) + 1,
                     rand.nextInt((gameSize - 1)) + 1);
             goblinList.add(goblin);
         }
@@ -76,5 +91,72 @@ public class Initializer {
                 }
             }
         }
+    }
+
+    /**
+     * Gets a name from a combination of random names from the human
+     * first and last name files
+     * @return String : name of the human
+     */
+    public String getHumanName() {
+        ArrayList<String> firstnames = new ArrayList<>();
+        ArrayList<String> lastnames = new ArrayList<>();
+        try {
+            String HUMAN_FIRST_NAMES = "HumanFirstNames.txt";
+            InputStream is = getClass().getClassLoader().getResourceAsStream(HUMAN_FIRST_NAMES);
+            assert is != null;
+            Stream<String> stream = new BufferedReader(new InputStreamReader(is)).lines();
+            firstnames = (ArrayList<String>) stream
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            System.out.println("Error reading file");
+        }
+        try {
+            String HUMAN_LAST_NAMES = "HumanLastNames.txt";
+            InputStream is = getClass().getClassLoader().getResourceAsStream(HUMAN_LAST_NAMES);
+            assert is != null;
+            Stream<String> stream = new BufferedReader(new InputStreamReader(is)).lines();
+            lastnames = (ArrayList<String>) stream
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            System.out.println("Error reading file");
+        }
+        return firstnames.get(new Random().nextInt(firstnames.size())) + " "
+                + lastnames.get(new Random().nextInt(lastnames.size()));
+    }
+    public String getGoblinName() {
+        ArrayList<String> firstnames = new ArrayList<>();
+        ArrayList<String> lastnames = new ArrayList<>();
+        try {
+            String GOBLIN_FIRST_NAMES = "GoblinFirstNames.txt";
+            InputStream is = getClass().getClassLoader().getResourceAsStream(GOBLIN_FIRST_NAMES);
+            assert is != null;
+            Stream<String> stream = new BufferedReader(new InputStreamReader(is)).lines();
+            firstnames = (ArrayList<String>) stream
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            System.out.println("Error reading file");
+        }
+        try {
+            String GOBLIN_LAST_NAMES = "GoblinLastNames.txt";
+            InputStream is = getClass().getClassLoader().getResourceAsStream(GOBLIN_LAST_NAMES);
+            assert is != null;
+            Stream<String> stream = new BufferedReader(new InputStreamReader(is)).lines();
+            lastnames = (ArrayList<String>) stream
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            System.out.println("Error reading file");
+        }
+        return firstnames.get(new Random().nextInt(firstnames.size())) + " "
+                + lastnames.get(new Random().nextInt(lastnames.size()));
+    }
+
+    public boolean doesNameExist(String name, ArrayList<Humanoid> humanoids) {
+        for (Humanoid humanoid : humanoids) {
+            if (humanoid.getName().equals(name)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
